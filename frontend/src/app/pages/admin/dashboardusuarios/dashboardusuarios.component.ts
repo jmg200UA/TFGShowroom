24a64e93b5ea4a0034d92d2b5b9cf5e2582f79fd
Javier Martín Gómez <jmg200@alu.ua.es>
2022-06-05 23:45:03 +0200

    borrarUsuario

--- frontend/src/app/pages/admin/dashboardusuarios/dashboardusuarios.component.ts
+++ frontend/src/app/pages/admin/dashboardusuarios/dashboardusuarios.component.ts
@@ -33,7 +33,7 @@ export class DashboardUsuariosComponent implements OnInit {
       .subscribe( res => {
         // Lo que nos llega lo asignamos a lista usuarios para renderizar la tabla
         // Comprobamos si estamos en un apágina vacia, si es así entonces retrocedemos una página si se puede
-
+        console.log("LA RES de usuarios: ", res['usuarios']);
         if (res['usuarios'].length === 0) {
           if (this.posicionactual > 0) {
             this.posicionactual = this.posicionactual - this.registrosporpagina;
@@ -52,6 +52,35 @@ export class DashboardUsuariosComponent implements OnInit {
         Swal.fire({icon: 'error', title: 'Oops...', text: 'No se pudo completar la acción, vuelva a intentarlo',});
         //console.warn('error:', err);
         this.loading = false;
+      });
+  }
+
+  eliminarUsuario( uid: string, nombre_apellidos: string) {
+    // Comprobar que no me borro a mi mismo
+    // if (uid === this.usuarioService.uid) {
+    //   Swal.fire({icon: 'warning', title: 'Oops...', text: 'No puedes eliminar tu propio usuario',});
+    //   return;
+    // }
+
+    Swal.fire({
+      title: 'Eliminar usuario',
+      text: `Al eliminar al usuario '${nombre_apellidos}' se perderán todos los datos asociados. ¿Desea continuar?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar'
+    }).then((result) => {
+          if (result.value) {
+            this.usuarioService.borrarUsuario(uid)
+              .subscribe( resp => {
+                this.cargarUsuarios(this.ultimaBusqueda);
+              }
+              ,(err) =>{
+                Swal.fire({icon: 'error', title: 'Oops...', text: 'No se pudo completar la acción, vuelva a intentarlo',});
+                console.warn('error:', err);
+              })
+          }
       });
   }
 
