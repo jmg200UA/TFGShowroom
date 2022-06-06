3ebb68e5233493b7bdefde2f7ff944f5c86616aa
Javier Martín Gómez <jmg200@alu.ua.es>
2022-06-06 11:47:36 +0200

    borrarTrabajo

--- frontend/src/app/pages/admin/dashboardtrabajos/dashboardtrabajos.component.ts
+++ frontend/src/app/pages/admin/dashboardtrabajos/dashboardtrabajos.component.ts
@@ -55,4 +55,27 @@ export class DashboardTrabajosComponent implements OnInit {
       });
   }
 
+  eliminarTrabajo( uid: string, titulo: string) {
+    Swal.fire({
+      title: 'Eliminar trabajo',
+      text: `Al eliminar el trabajo '${titulo}' se perderán todos los datos asociados. ¿Desea continuar?`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, borrar'
+    }).then((result) => {
+          if (result.value) {
+            this.trabajoService.borrarTrabajo(uid)
+              .subscribe( resp => {
+                this.cargarTrabajos(this.ultimaBusqueda);
+              }
+              ,(err) =>{
+                Swal.fire({icon: 'error', title: 'Oops...', text: 'No se pudo completar la acción, vuelva a intentarlo',});
+                console.warn('error:', err);
+              })
+          }
+      });
+  }
+
 }
