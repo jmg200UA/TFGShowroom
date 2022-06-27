c435b4d76a8898124380ee7137762fd35f6991a5
Javier Martín Gómez <jmg200@alu.ua.es>
2022-06-27 20:11:36 +0200

    landing background y estilo

--- frontend/src/app/pages/landing/inicio/inicio.component.ts
+++ frontend/src/app/pages/landing/inicio/inicio.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
+
 
 @Component({
   selector: 'inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css',
+              "../../../../../node_modules/keen-slider/keen-slider.min.css"]
 })
 export class InicioComponent implements OnInit {
 
@@ -11,5 +14,21 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @ViewChild("sliderRef") sliderRef: ElementRef<HTMLElement>
+
+  slider: KeenSliderInstance = null
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      loop: true,
+      rtl: true,
+      slides: {
+        perView: 3,
+        spacing: 10,
+      },
+    })
+  }
+
 
 }
