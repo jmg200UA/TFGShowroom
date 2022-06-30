377bd08490d83beb3d3cab5b0abdad52cbbc80f6
Javier Martín Gómez <jmg200@alu.ua.es>
2022-06-30 14:28:27 +0200

    ficha trabajos

--- frontend/src/app/pages/landing/detallestrabajo/detallestrabajo.component.ts
+++ frontend/src/app/pages/landing/detallestrabajo/detallestrabajo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { TrabajosService } from '../../../services/trabajos.service';
 
 @Component({
   selector: 'detallestrabajo',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallestrabajoComponent implements OnInit {
 
-  constructor() { }
+  private uid: string = '';
+  public loading = true;
+  public trabajo;
+
+  constructor(private fb: FormBuilder,
+              private route: ActivatedRoute,
+              private router: Router,
+              private TrabajosService: TrabajosService) { }
 
   ngOnInit(): void {
+    this.uid = this.route.snapshot.params['uid'];
+    console.log("UID: ", this.uid);
+    this.cargarTrabajo();
+  }
+
+  cargarTrabajo(){
+    this.loading = true;
+    this.TrabajosService.obtenerTrabajo(this.uid)
+      .subscribe( res => {
+        console.log("LA RES de trabajos: ", res['trabajos']);
+          this.trabajo = res['trabajos'];
+          this.loading=false;
+      }, (err) => {
+        Swal.fire({icon: 'error', title: 'Oops...', text: 'No se pudo completar la acción, vuelva a intentarlo',});
+        //console.warn('error:', err);
+        this.loading = false;
+      });
+  }
+
+  crearImagenUrl(imagen: string) {
+    return this.TrabajosService.crearImagenUrl(imagen);
   }
 
 }
