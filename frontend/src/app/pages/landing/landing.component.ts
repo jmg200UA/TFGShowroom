e9a475461740cc9fbf521d47bc9ee9fbf5babaf9
Javier Martín Gómez <jmg200@alu.ua.es>
2022-07-02 14:54:02 +0200

    titulaciones landing

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {UsuarioService} from '../../services/usuario.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { TitulacionService } from '../../services/titulacion.service';
 
 @Component({
   selector: 'landing',
@@ -12,16 +14,48 @@ export class LandingComponent implements OnInit {
 
   public rol="";
 
+  //Variables para la carga de titulaciones
+  public listaTitulaciones;
+  public loading = true;
+  public listatitu: boolean = true;
+
   constructor(private UsuarioService: UsuarioService,
-              private router: Router) { }
+              private router: Router,
+              private TitulacionService: TitulacionService) { }
 
   ngOnInit(): void {
     //this.rol= this.UsuarioService.rol;
     this.router.navigateByUrl('/landing/inicio');
+    this.cargarTitulaciones();
   }
 
   logout() {
     this.UsuarioService.logout();
+  }
+
+  //cargamos las titulaciones
+  cargarTitulaciones() {
+    this.loading = true;
+    this.TitulacionService.cargarTitulacionesTodo()
+      .subscribe( res => {
+          this.listaTitulaciones = res['titulaciones'];
+          this.loading = false;
+        }, (err) => {
+          Swal.fire({icon: 'error', title: 'Oops...', text: 'No se pudo completar la acción, vuelva a intentarlo',});
+          this.loading = false;
+        });
+    }
+
+  //Para cargar la imagen de la titulacion
+  crearUrlTitulacion(imagen:string){
+    return this.TitulacionService.crearImagenUrl(imagen);
+  }
+
+
+  cambiotitu(){
+    if(this.listatitu) this.listatitu= false;
+    else this.listatitu = true;
+    console.log(this.listatitu);
   }
 
 }
