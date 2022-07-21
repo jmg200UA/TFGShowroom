144ff9ee63596150f89990e5e212813889e5c288
Javier Martín Gómez <jmg200@alu.ua.es>
2022-07-21 15:57:45 +0200

    intentando ident google

--- frontend/src/app/pages/landing/detallestrabajo/detallestrabajo.component.ts
+++ frontend/src/app/pages/landing/detallestrabajo/detallestrabajo.component.ts
@@ -23,7 +23,7 @@ declare const gapi:any;
   styleUrls: ['./detallestrabajo.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class DetallestrabajoComponent implements OnInit, AfterContentInit {
+export class DetallestrabajoComponent implements OnInit {
 
   @ViewChild('closeAddExpenseModal') public closeAddExpenseModal: ElementRef;
 
@@ -67,9 +67,6 @@ export class DetallestrabajoComponent implements OnInit, AfterContentInit {
     this.cargarTrabajo();
   }
 
-  public ngAfterContentInit(): void {
-    this.startApp();
- }
 
   cargarTrabajo(){
     this.loading = true;
@@ -132,43 +129,49 @@ export class DetallestrabajoComponent implements OnInit, AfterContentInit {
 
 // Para votar
 
-startApp() {
-  gapi.load('auth2', ()=>{
-    // Retrieve the singleton for the GoogleAuth library and set up the client.
-    this.auth2 = gapi.auth2.init({
-      client_id: '826053114073-g8rqruj8i8bl3gubhrlfa3juc27ffkrn.apps.googleusercontent.com',
-      cookiepolicy: 'single_host_origin',
-    });
-    this.attachSignin(document.getElementById('my-signin2'));
-  });
-};
+// startApp() {
+//   gapi.load('auth2', ()=>{
+//     // Retrieve the singleton for the GoogleAuth library and set up the client.
+//     this.auth2 = gapi.auth2.init({
+//       client_id: '826053114073-g8rqruj8i8bl3gubhrlfa3juc27ffkrn.apps.googleusercontent.com',
+//       cookiepolicy: 'single_host_origin',
+//     });
+//     this.attachSignin(document.getElementById('my-signin2'));
+//   });
+// };
 
-attachSignin(element) {
-  if(element!=null){
-    this.auth2.attachClickHandler(element, {},
-    (googleUser) => {
-      var id_token = googleUser.getAuthResponse().id_token;
-      this.closeAddExpenseModal.nativeElement.click();
-      this.TrabajosService.loginGoogle(id_token)
-        .subscribe( res => {
-          this.zone.run(() => {
-            this.router.navigateByUrl('/admin/dashboard');
-          });
-        }, (err) => {
-          console.warn('Error respueta api:',err);
-          Swal.fire({
-            title: 'Error!',
-            text: err.error.msg || 'No pudo completarse la acción, vuelva a intentarlo más tarde',
-            icon: 'error',
-            confirmButtonText: 'Ok',
-            allowOutsideClick: false
-          });
-        }
-        );
-    }, (error) => {
-      alert(JSON.stringify(error, undefined, 2));
-    });
-  }
+// attachSignin(element) {
+//   if(element!=null){
+//     this.auth2.attachClickHandler(element, {},
+//     (googleUser) => {
+//       var id_token = googleUser.getAuthResponse().id_token;
+//       this.closeAddExpenseModal.nativeElement.click();
+//       this.TrabajosService.loginGoogle(id_token)
+//         .subscribe( res => {
+//           this.zone.run(() => {
+//             this.router.navigateByUrl('/admin/dashboard');
+//           });
+//         }, (err) => {
+//           console.warn('Error respueta api:',err);
+//           Swal.fire({
+//             title: 'Error!',
+//             text: err.error.msg || 'No pudo completarse la acción, vuelva a intentarlo más tarde',
+//             icon: 'error',
+//             confirmButtonText: 'Ok',
+//             allowOutsideClick: false
+//           });
+//         }
+//         );
+//     }, (error) => {
+//       alert(JSON.stringify(error, undefined, 2));
+//     });
+//   }
+// }
+
+onSignIn(googleUser) {
+  console.log("entro");
+  var profile = googleUser.getBasicProfile();
+  console.log("Profile: ", profile);
 }
 
 
